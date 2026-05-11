8e9e7aa7b404b3f9d5cc70e7b5ce99b8ac641fbe
dindaaulia9 <dindaaulyaa9@gmail.com>
2026-05-11 20:47:04 +0700

    Update Produk.java

--- Produk.java
+++ Produk.java
@@ -3,37 +3,31 @@ public abstract class Produk {
     private double harga;
     private int stok;
 
-    public Produk(String nama, double harga, int stok) {
-        this.nama = nama;
-        this.harga = harga;
-        this.stok = stok;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public double getHarga() {
-        return harga;
-    }
-
-    public int getStok() {
-        return stok;
-    }
-
-    public void setStok(int stok) {
-        this.stok = stok;
-    }
-
-    public void tampilkan() {
-        System.out.println("Nama: " + nama);
-        System.out.println("Harga: " + harga);
-        System.out.println("Stok: " + stok);
-    }
-
-    public double hitungHarga(int jumlah) {
-        return jumlah * harga;
-    }
-
-    public abstract void hitungDiskon(int jumlah);
+public Produk (String nama, double harga, int stok){
+    this.nama = nama;
+    this.harga = harga;
+    this.stok = stok;
 }
+
+public int getStok() {
+    return stok;
+}
+
+public void setStok(int jumlah){
+    stok = stok - jumlah;
+
+}
+
+public void tampilInfo(){
+    System.out.println("nama:" + nama);
+    System.out.println("harga:" + harga);
+    System.out.println("stok:" + stok);
+}
+
+public double hitungTotalHarga(int jumlah) {
+    return jumlah * harga;
+}
+
+public abstract double hitungTotalBayar(int jumlah);
+}
+
